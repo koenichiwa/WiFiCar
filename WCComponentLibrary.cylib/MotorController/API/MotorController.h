6a09a3d38cc7631b7ad3e826117ef5bcee30f9b8
Koenichiwa <koenvwel@gmail.com>
2020-11-10 12:48:50 +0100

    Add MotorController Component
    Still need to assign thne pin positions

---
+++ WCComponentLibrary.cylib/MotorController/API/MotorController.h
@@ -0,0 +1,17 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#pragma once
+#include "cytypes.h"
+void `$INSTANCE_NAME`_Start(void);
+void `$INSTANCE_NAME`_SetSpeeds(int16 left, int16 right);
+void `$INSTANCE_NAME`_Stop(void);
+/* [] END OF FILE */
