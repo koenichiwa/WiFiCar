73597a19e4b3da71aa40966e7ccf02cfc34c97fc
Koenichiwa <koenvwel@gmail.com>
2020-11-10 17:06:12 +0100

    Add UART for testing and main bulk off code

--- Car.cydsn/main.c
+++ Car.cydsn/main.c
@@ -9,18 +9,294 @@
  *
  * ========================================
 */
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
 #include "project.h"
+#include <math.h>
+#include <stdio.h>
+#include <assert.h>
+#include "squircular.h"
 
+//MACROS
+
+// Not used anymore but still useful
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+    
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+    
+//FLAGS
+#define FLAG_STATE_CHANGED  0b00000001
+#define FLAG_STATE_QUIT     0b00000010
+// insert other flags here
+#define FLAG_STATE_TEST     0b10000000
+
+//CONSTANTS
+//-OLED
+#define OLED_LINE_COMMAND 1
+#define OLED_LINE_SPEED 3
+// 5 not in use *yet*
+#define OLED_LINE_EXTRA 7
+
+//-UART
+#define UART_COMMAND_DRIVE  'd'
+#define UART_COMMAND_LIGHT  'l'
+#define UART_COMMAND_TEST   't'
+#define UART_COMMAND_QUIT   'q'
+
+#define UART_RESPONSE_MSG_UNKNOWN   'u'
+
+//-DRIVECOMMAND
+#define DRIVECOMMAND_SPACE_CIRCLE 'c'
+#define DRIVECOMMAND_SPACE_SQUARE 's'
+
+
+//STRUCT DEFINITIONS AND STRUCT SPECIFIC FUNCTIONS
+typedef struct driveCommand {
+    char space; // 'c' denotes vector is on a circle 's' denotes it's on a square
+    float forward;
+    float direction; //Positive is right negative is left
+} driveCommand_t;
+
+typedef struct state {
+    driveCommand_t driveCommand;
+    uint8 flags;
+} state_t;
+
+typedef struct speeds {
+    int16 left;
+    int16 right;
+} speeds_t;
+
+//FUNCTION PROTOTYPES
+void initialize();
+void terminate();
+driveCommand_t readDriveCommand();
+speeds_t convertDriveCommand(driveCommand_t command);
+
+//-INTERRUPT PROTOTYPES
+CY_ISR_PROTO(commandReceived);
+
+//-FOR TESTING PURPOSES
+void runTestRoutine(int delayMs);
+int testCounter = 0;
+
+//GLOBAL VARIABLES
+volatile state_t state = {
+    {DRIVECOMMAND_SPACE_SQUARE, 0.0, 0.0}, // driveCommand
+    FLAG_STATE_CHANGED // hasChanged (Set to 1 because "The creation of state is a change of state" - <INSERT RANDOM PHILOSOPHER>)
+};
+
+//MAIN
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
+    initialize();
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
-    for(;;)
+    while(1)
     {
-        /* Place your application code here. */
+        if(state.flags & FLAG_STATE_TEST) {
+            runTestRoutine(1000);
+        }
+        
+        if(state.flags & FLAG_STATE_CHANGED) {
+            speeds_t speeds = convertDriveCommand(state.driveCommand);
+            MotorController_SetSpeeds(speeds.left, speeds.right);
+            state.flags = state.flags & ~FLAG_STATE_CHANGED;
+        }
+        
+        if(state.flags & FLAG_STATE_QUIT){
+            terminate();
+        }
     }
+}
+
+//HELPER FUNCTION PREDECLERATIONS
+//-INIT FUNCTIONS
+void initUart();
+void terminUart();
+
+//INTERRUPT DEFINITIONS
+CY_ISR(commandReceived){
+    char commandIdentifier = UART_GetChar();
+
+    switch(commandIdentifier){
+        case UART_COMMAND_DRIVE:
+            state.driveCommand = readDriveCommand();
+            state.flags = state.flags | FLAG_STATE_CHANGED;
+            break;
+        case UART_COMMAND_LIGHT: //Switch Light (for testing connection)
+            Out_LED_Write(~Out_LED_Read());
+            break;
+        case UART_COMMAND_TEST:
+            state.flags = state.flags | FLAG_STATE_TEST;
+            break;
+        case UART_COMMAND_QUIT:
+            state.flags = state.flags | FLAG_STATE_QUIT;
+            break;
+        default: //Signal that an unknown message was received.
+            UART_PutChar(UART_RESPONSE_MSG_UNKNOWN);
+            
+    }
+}
+
+//FUNCTION DEFINITIONS
+//-TEST ROUTINE
+void runTestRoutine(int delay){
+    Out_LED_Write(1);
+    switch(testCounter){
+        case 0:
+            state.driveCommand.direction = 1.0;
+            state.driveCommand.direction = 0.0;
+            state.flags = state.flags | FLAG_STATE_CHANGED;
+            testCounter++;
+            break;
+        case 1:
+            CyDelay(delay);
+            state.driveCommand.direction = -1.0;
+            state.driveCommand.direction = 0.0;
+            state.flags = state.flags | FLAG_STATE_CHANGED;
+            testCounter++;
+            break;
+        case 2:
+            CyDelay(delay);
+            state.driveCommand.direction = 0.0;
+            state.driveCommand.direction = 1.0;
+            state.flags = state.flags | FLAG_STATE_CHANGED;
+            testCounter++;
+            break;
+        case 3:
+            CyDelay(delay);
+            state.driveCommand.direction = 0.0;
+            state.driveCommand.direction = -1.0;
+            state.flags = state.flags | FLAG_STATE_CHANGED;
+            testCounter++;
+            break;
+        case 4:
+            CyDelay(delay);
+            state.driveCommand.direction = 1.0;
+            state.driveCommand.direction = 1.0;
+            state.flags = state.flags | FLAG_STATE_CHANGED;
+            testCounter++;
+            break;
+        case 5:
+            CyDelay(delay);
+            state.driveCommand.direction = -1.0;
+            state.driveCommand.direction = -1.0;
+            state.flags = state.flags | FLAG_STATE_CHANGED;
+            testCounter++;
+            break;
+        case 6:
+            CyDelay(delay);
+            state.driveCommand.direction = 0.5;
+            state.driveCommand.direction = 0.0;
+            state.flags = state.flags | FLAG_STATE_CHANGED;
+            testCounter++;
+            break;
+        default:
+            CyDelay(delay);
+            state.driveCommand.direction = 0.0;
+            state.driveCommand.direction = 0.0;
+            testCounter = 0;
+            state.flags = (state.flags | FLAG_STATE_CHANGED) & ~FLAG_STATE_TEST ;
+            Out_LED_Write(0);
+    }
+    
+}
+
+
+//-INIT FUNCTIONS
+void initialize(){
+    MotorController_Start();
+    initUart();
+}
+
+void terminate(){
+    MotorController_Stop();
+    terminUart();
+    CyPmHibernate();
+}
+
+void initUart(){
+    InterruptCommandReceived_StartEx(commandReceived);
+    UART_Start();
+}
+
+void terminUart(){
+    InterruptCommandReceived_Stop();
+    UART_Stop();
+}
+
+//-UART FUNCTIONS
+driveCommand_t readDriveCommand(){
+    driveCommand_t command;
+    
+    char* pointer = (char*) &command;
+    for(unsigned int i = 0; i<sizeof(driveCommand_t); i++){
+        pointer[i] = UART_GetChar();
+    }
+    
+    return command;
+}
+
+
+//-MOTOR FUNCTIONS
+
+/**
+ * This function transforms a vector (direction, forward), which is found in driveCommand, to the vector (left, right) which represents 
+ * the speed at which the left and the right motors run.
+ * This can be achieved by rotating the vector by 45 degrees and projecting it on a square. (if (f,d) == (1.0, 0.0) then (l,r) == (1.0, 1.0))
+ * (direction, forward) is expected to be on or inside the unit circle when command.space == 'c'
+ * (direction, forward) is expected to be on or inside the square with range ([-1.0, 1.0], [-1.0, 1.0]) command.space == 's'
+ * This is not tested during runtime (except if ran in debug mode)
+ * If command.space is anything else it will return (0.0, 0.0)
+ *
+ * @param command The drive command containing the vector (f,d) and the space on which they lie
+ * @return The vector (l,r) In the range [-255,255]
+**/
+speeds_t convertDriveCommand(driveCommand_t command){
+    float circledDirection;
+    float circledForward;
+    
+    // project on circle if needed
+    if (command.space == DRIVECOMMAND_SPACE_SQUARE){
+        ellipticalSquareToDisc(command.direction, command.forward, &circledDirection, &circledForward);
+    }
+    else if(command.space == DRIVECOMMAND_SPACE_CIRCLE){
+        circledDirection = command.direction;
+        circledForward = command.forward;
+    } else {
+        //invalid
+        speeds_t speeds = {0,0};
+        return speeds;
+    }
+    assert((circledDirection*circledDirection + circledForward*circledForward) <= 1.0);
+    //rotate by 45 degrees
+    float right = (circledForward - circledDirection) * M_SQRT1_2;
+    float left = (circledForward + circledDirection) * M_SQRT1_2;
+    float mappedLeft;
+    float mappedRight;
+    
+    //project back on square
+    ellipticalDiscToSquare(left, right, &mappedLeft, &mappedRight);
+    speeds_t speeds;
+    speeds.left = (int16) (mappedLeft * 255.0);
+    speeds.right = (int16) (mappedRight* 255.0);
+    return speeds;
 }
 
 /* [] END OF FILE */
---
+++ Car.cydsn/squircular.h
@@ -0,0 +1,49 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#pragma once
+
+
+#include <stdio.h>
+#include "gsl_math.h"
+
+// Elliptical Grid mapping
+// mapping a circular disc to a square region
+// input: (u,v) coordinates in the circle
+// output: (x,y) coordinates in the square
+void ellipticalDiscToSquare(float u, float v, float* x, float* y)
+{
+    float u2 = u * u;
+    float v2 = v * v;
+    
+    float twosqrt2 = 2.0 * M_SQRT2;
+    float subtermx = 2.0 + u2 - v2;
+    float subtermy = 2.0 - u2 + v2;
+    float termx1 = subtermx + u * twosqrt2;
+    float termx2 = subtermx - u * twosqrt2;
+    float termy1 = subtermy + v * twosqrt2;
+    float termy2 = subtermy - v * twosqrt2;
+    *x = 0.5 * sqrt(termx1) - 0.5 * sqrt(termx2);
+    *y = 0.5 * sqrt(termy1) - 0.5 * sqrt(termy2);
+    
+}
+
+
+// Elliptical Grid mapping
+// mapping a square region to a circular disc
+// input: (x,y) coordinates in the square
+// output: (u,v) coordinates in the circle
+void ellipticalSquareToDisc(float x, float y, float* u, float* v)
+{
+    *u = x * sqrt(1.0 - y*y/2.0);
+    *v = y * sqrt(1.0 - x*x/2.0);    
+}
+/* [] END OF FILE */
