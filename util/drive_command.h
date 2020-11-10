d5647b4f026094ae0646312f0c682fda1a5fd73f
Koen <koenvwel@gmail.com>
2020-11-10 23:47:03 +0100

    Add driveCommandToCharArray and back

--- util/drive_command.h
+++ util/drive_command.h
@@ -3,11 +3,36 @@
  */
 #pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "vector.h"
+
 #define DC_CIRCLE_DOMAIN 'c'
 #define DC_SQUARE_DOMAIN 's'
 
-typedef struct drive_command {
+typedef struct driveCommand {
     char domain; // 'c' denotes vector is on a circle 's' denotes it's on a square
     fvector2_t moveVector; // x is direction, y is forward
-} drive_command_t;
+} driveCommand_t;
+
+void driveCommandToCharArray(driveCommand_t * in, unsigned char* out){
+    for(int i; i<sizeof(driveCommand_t); i++)
+        out[i] = ((unsigned char*) in)[i];
+}
+
+driveCommand_t charArrayToDriveCommand(unsigned char* in){
+    union catdb {
+        unsigned char bytes[sizeof(driveCommand_t)];
+        driveCommand_t command;
+    } catdb_i;
+    for (int i = 0; i < sizeof(driveCommand_t); ++i) {
+        catdb_i.bytes[i] = in[i];
+    }
+    return catdb_i.command;
+}
+
+#ifdef __cplusplus
+}
+#endif
